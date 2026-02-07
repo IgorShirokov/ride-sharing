63bc04f8a53777214522d1dbac4cc6feefffcb2b
IgorShirokov <shirokov-iv@mail.ru>
2026-02-07 06:32:36 +0300

    init

---
+++ services/trip-service/internal/domain/trip.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"context"
+	"ride-sharing/shared/types"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TripModel struct {
+	ID       primitive.ObjectID
+	UserID   string // id водителя
+	Status   string
+	RideFare *RideFareModel // тариф
+}
+
+type TripRepository interface {
+	CreateTrip(ctx context.Context, trip *TripModel) (*TripModel, error)
+}
+
+type TripService interface {
+	CreateTrip(ctx context.Context, fare *RideFareModel) (*TripModel, error)
+	GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*types.OsrmApiResponse, error)
+}
